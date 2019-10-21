dd8824901e8cc26587a102402653043f7b67ff6c
Tracy Pineda <tracypineda@gmail.com>
2019-10-21 12:11:22 -0600

    morning work, authController and routes and tables

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO Have (itemName, itemDescription, itemCategory)
+VALUES ("shirt", "blue and red", "clothing"), ("computer", "very old", "electronics"),("iPhone", "5s", "electronics");
+
+INSERT INTO Want (itemName, itemDescription, itemCategory)
+VALUES ("shirt", "blue and red", "clothing"), ("computer", "very old", "electronics"),("iPhone", "5s", "electronics");
+
+INSERT INTO User (email, password)
+VALUES ("tracypineda@gmail.com", "testing");
