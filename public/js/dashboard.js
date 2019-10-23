98b9640a8502297a1f2c07f59a17edaecf4b60ad
Tracy Pineda <tracypineda@gmail.com>
2019-10-23 10:40:24 -0600

    working on dashboard js

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,26 +1,27 @@
 // add a Want
 
-$("#newItem").on("submit", function(event) {
+$("#newItem").on("click", function (event) {
     // Make sure to preventDefault on a submit event.
     event.preventDefault();
-
+ console.log("hello");
+ 
     var newItem = {
-      itemName: $("#itemName").val().trim(),
-      itemDescription: $("itemDescription").val().trim(),
-      itemCategory: $("#itemCategory").val().trim(),
-      itemPhoto: $("itemPhoto").val().trim()
+        itemName: $("#itemName").val().trim(),
+        itemDescription: $("itemDescription").val().trim(),
+        itemCategory: $("#itemCategory").val().trim(),
+        itemPhoto: $("itemPhoto").val().trim()
     };
-  
+    console.log(newItem);
     var type = $("#havewant" `[name = ${type}]:checked`).val();
-​
-if ( type.toLowerCase() === "have") { 
-//Add the item to Haves table 
-​
-	$.ajax ({
-		url: (`/api/${type}`),
-		method: "POST",
-		data: newItem
-	});
-}
-else { 
-}//perform a different Ajax call or any other action u would like it to do 
+
+    if (type.toLowerCase() === "have") {
+        //Add the item to Haves table 
+
+        $.ajax({
+            url: (`/api/${type}`),
+            method: "POST",
+            data: newItem
+        });
+    }
+    else {
+    }//perform a different Ajax call or any other action u would like it to do 
