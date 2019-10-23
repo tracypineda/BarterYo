9b5735512e85170d4401e25eda0b22af48766262
Tracy Pineda <tracypineda@gmail.com>
2019-10-23 10:00:56 -0600

    fixed radio buttons

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,6 @@
 DROP DATABASE IF EXISTS sequelize_barter_db;
 CREATE DATABASE sequelize_barter_db;
 USE sequelize_barter_db;
+SELECT * FROM haves;
+SELECT * FROM wants;
+SELECT * FROM users;
--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,7 +1,5 @@
 // add a Want
 
-
-
 $("#newItem").on("submit", function(event) {
     // Make sure to preventDefault on a submit event.
     event.preventDefault();
@@ -12,21 +10,8 @@ $("#newItem").on("submit", function(event) {
       itemCategory: $("#itemCategory").val().trim(),
       itemPhoto: $("itemPhoto").val().trim()
     };
-});
-// google how to get the value of the radio button
-var type = $("#havewant[name = ${type}]`:checked").val();
-$.ajax ({
-    url: (`/api/${type}`),
-    method: "POST",
-    data: newItem
-});
-
-$("#itemName").val("");
-$("#itemnDescription").val("");
-$("#itemCategory").val("");
-$("#itemPhoto").val("");
-
-    var type = $("#havewant `[name = ${type}]`:checked").val();
+  
+    var type = $("#havewant" `[name = ${type}]:checked`).val();
 ​
 if ( type.toLowerCase() === "have") { 
 //Add the item to Haves table 
@@ -38,24 +23,4 @@ if ( type.toLowerCase() === "have") {
 	});
 }
 else { 
-    (type === undefined) {
-        console.log("please choose a item type!");
-    }
-    
-	//perform a different Ajax call or any other action u would like it to do 
-​
-}
-
-
-$.ajax ({
-    url:("/api/have"),
-    method: "POST",
-    data: Have
-});
-$.ajax({
-    url:("/api/want"),
-    method: "POST",
-    data: Want
-});
-
-
+}//perform a different Ajax call or any other action u would like it to do 
